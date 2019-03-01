cea2fc433b345b600b3dd79e6e5b977f36b6da03
Marc Dwyer <marcdwyer1738@gmail.com>
2019-03-01 11:29:08 -0600

    init

---
+++ src/components/main/index.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const Padre = styled.div`
+width: 100%;
+height: 100vh;
+display: flex;
+`
+export const Hijo = styled.div`
+width: 75%;
+margin: 0 auto 0 auto;
+`
---
+++ src/components/main/main.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Padre, Hijo } from './index'
+
+const Main = () => {
+    return (
+        <Padre>
+            <Hijo>
+                Hello
+            </Hijo>
+        </Padre>
+    )
+}
+
+export default Main
